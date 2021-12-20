5760f7c3678c573219f95e2b76c281130ed53aa8
oteseff <iobyhs@gmail.com>
2021-12-19 21:05:29 -0800

    Remove variables

--- main.tf
+++ main.tf
@@ -21,10 +21,6 @@ module "bootstrap" {
   source                      = "./modules/bootstrap"
   name_of_s3_bucket           = "actions-terraform-aws-tfstate"
   dynamo_db_table_name        = "aws-locks"
-  #iam_user_name               = "GitHubActionsIamUser"
-  #ado_iam_role_name           = "GitHubActionsIamRole"
-  #aws_iam_policy_permits_name = "GitHubActionsIamPolicyPermits"
-  #aws_iam_policy_assume_name  = "GitHubActionsIamPolicyAssume"
 }
 
 /*
